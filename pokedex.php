f802dcc121d80f21fdf56a6b4a8cdeaf1fd5fd58
msaez97 <miguelangelsaezmateo@gmail.com>
2023-12-13 19:50:11 +0100

    acento

--- pokedex.php
+++ pokedex.php
@@ -14,7 +14,7 @@
 	<?php require 'assets/header.php'; ?>
 	<main class="contenedor">
 		<h1 class="h1-style">Pokedex Wiki PokeMMO No Oficial en Español</h1>
-		<p class="padding-no-bottom">¡Bienvenido a esta wiki no oficial en español de PokeMMO! Aquí encontrarás una valiosa fuente de información sobre cada Pokémon de este multijugador en linea.</p>
+		<p class="padding-no-bottom">¡Bienvenido a esta wiki no oficial en español de PokeMMO! Aquí encontrarás una valiosa fuente de información sobre cada Pokémon de este multijugador en línea.</p>
 		<div class="buscador">
 			<input oninput="mostrarSugerencias()" class="input-buscador" type="text" placeholder="Buscar pokemon por nombre..">
 			<div class="sugerencias" id="sugerenciasContainer"></div>
