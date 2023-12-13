b1ad24d3629ccb17a2099f00ef1a66fef4a431b4
msaez97 <miguelangelsaezmateo@gmail.com>
2023-12-13 13:37:59 +0100

    head pokemon.php

--- pokemon.php
+++ pokemon.php
@@ -1,12 +1,18 @@
+<?php 
+$pokemonName = $_GET["name"];
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pokemon</title>
+    <title><?php echo $pokemonName; ?> | Wiki | PokeMMO | Español | No Oficial</title>
 </head>
 <body>
-    
+    <?php require 'assets/header.php'; ?>
+
+
+
     <script src="js/pokemon.js"></script>
 </body>
 </html>
