483f7773d0a808f0a472789f52e90a51f5c9fc5a
msaez97 <miguelangelsaezmateo@gmail.com>
2023-12-13 19:24:19 +0100

    meta description

--- pokedex.php
+++ pokedex.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Mira la pokedex de la Wiki de PokeMMO no oficial, encuentra cualquier pokemon, totalmente en español.">
     <title>Wiki | Pokedex | PokeMMO | Español | No Oficial</title>
-	<link rel="icon" type="image/vnd.icon" href="img/favicon.ico">
+	<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="img/favicon.ico">
     <link rel="stylesheet" href="css/style.css">
     <link rel="preload" href="js/script.js" as="script" />
 </head>
