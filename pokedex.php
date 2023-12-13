a98aa45557aff481ada326cccb4140dc507df467
msaez97 <miguelangelsaezmateo@gmail.com>
2023-12-13 14:59:31 +0100

    boton

--- pokedex.php
+++ pokedex.php
@@ -16,7 +16,7 @@
 			<button onclick="buscarPokemon()">Buscar</button>
 		</div>
 		<div class="contenedor-mostrar-todo">
-			<button class="padding-all upper negro mostrartodo"><a href="/pokedex">Mostrar Todo</a></button>
+			<a class="padding-all upper negro mostrartodo" href="/pokedex">Mostrar Todo</a>
 		</div>
 		<div id="pokemonList"></div>
 		<div id="loadingIndicator">Cargando...</div>
