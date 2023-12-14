02b8ff31fa9bf286b4273188068da15196303972
msaez97 <miguelangelsaezmateo@gmail.com>
2023-12-14 13:11:39 +0100

    inicio

--- index.php
+++ index.php
@@ -19,7 +19,49 @@
 <body>
     <?php require 'assets/header.php'; ?>
 	<main class="contenedor">
-		
+		<h1>Bienvenidos a la Wiki de PokeMMO No Oficial en Español</h1>
+        <p>La primera wiki para la comunidad de habla hispana de PokeMMO.</p>
+        <p>Puedes encontrar desde guias de las regiones, pasando por una pokedex moderna e intuitiva, hasta crianza y equipos PVP de la comunidad.</p>
+        <p><strong>Links Oficiales del Juego y Redes:</strong> <a href="https://pokemmo.com/es/" target="_blank">Página Web</a> - <a href="https://forums.pokemmo.com/" target="_blank">Foros</a> - <a href="https://www.facebook.com/PokeMMORPG" target="_blank">Facebook</a> - <a href="https://twitter.com/pokemmo" target="_blank">Twitter</a> - <a href="https://www.reddit.com/r/pokemmo/" target="_blank">Reddit</a> - <a href="https://discord.com/invite/zXkuuDNYJh" target="_blank">Discord</a></p>
+        <div class="grid-index">
+            <div class="juegoCarta">
+                <div class="juegoHeader">
+                    <div class="juegoBack"></div>
+                    <h2>Sobre el Juego</h2>
+                    <img src="" alt="profesor Oak">
+                </div>
+                <div class="juegoDatos">
+
+                </div>
+            </div>
+            <div class="actualizacionesCarta">
+
+            </div>
+            <div class="pokedexCarta">
+
+            </div>
+            <div class="objetosCarta">
+
+            </div>
+            <div class="regionesCarta">
+
+            </div>
+            <div class="localizacionesCarta">
+
+            </div>
+            <div class="npcsCarta">
+
+            </div>
+            <div class="mecanicasCarta">
+
+            </div>
+            <div class="eventosCarta">
+
+            </div>
+            <div class="extensionesCarta">
+
+            </div>
+        </div>
 	</main>
 </body>
 </html>
