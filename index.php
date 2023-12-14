9dd06b0ad6df10822263060523ab92e74e95538b
msaez97 <miguelangelsaezmateo@gmail.com>
2023-12-14 16:01:45 +0100

    cambios inicio

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Wiki No Oficial de PokeMMO en Español, Todo Tipo de Guias, Pokedex Moderna, y Todo Sobre el Multijugador Online.">
     <title>Wiki | PokeMMO | Español | No Oficial</title>
 	<link rel="icon" type="image/vnd.icon" href="img/favicon.ico">
+	<link rel="apple-touch-icon" href="img/favicon.ico">
     <link rel="stylesheet" href="css/style.css">
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-GW3K77TW0W"></script>
@@ -24,42 +26,191 @@
         <p>Puedes encontrar desde guias de las regiones, pasando por una pokedex moderna e intuitiva, hasta crianza y equipos PVP de la comunidad.</p>
         <p class="enlacesOficiales"><strong>Links Oficiales del Juego y Redes:</strong> <a href="https://pokemmo.com/es/" target="_blank">Página Web</a> - <a href="https://forums.pokemmo.com/" target="_blank">Foros</a> - <a href="https://www.facebook.com/PokeMMORPG" target="_blank">Facebook</a> - <a href="https://twitter.com/pokemmo" target="_blank">Twitter</a> - <a href="https://www.reddit.com/r/pokemmo/" target="_blank">Reddit</a> - <a href="https://discord.com/invite/zXkuuDNYJh" target="_blank">Discord</a></p>
         <div class="grid-index">
-            <div class="juegoCarta">
+            <div class="contenedorDatos">
                 <div class="juegoHeader">
-                    <div class="juegoBack"></div>
-                    <h2 class="juegoH2"><span class="juegoH2Letra">S</span>obre el Juego</h2>
+                    <div class="backWidth Fuego"></div>
+                    <h2 class="juegoH2"><span class="FuegoColor">S</span>obre el Juego</h2>
                     <img src="img/profesor_oak.png" alt="profesor Oak" width="32" height="48">
                 </div>
                 <div class="juegoDatos">
-                    <p>Instalar Juego</p>
+                    <a class="enlaceDatosSobre" href="">Instalar Juego</a>
+                    <a class="enlaceDatosSobre" href="">Objetos</a>
+                    <a class="enlaceDatosSobre" href="">Crianza</a>
+                    <a class="enlaceDatosSobre" href="">Tienda</a>
+                    <a class="enlaceDatosSobre" href="">Regiones</a>
+                    <a class="enlaceDatosSobre" href="">Bayas</a>
+                    <a class="enlaceDatosSobre" href="">Trading</a>
+                    <a class="enlaceDatosSobre" href="">Pokedex</a>
+                    <a class="enlaceDatosSobre" href="">Crafteo</a>
+                    <a class="enlaceDatosSobre" href="">PVP</a>
                 </div>
             </div>
-            <div class="actualizacionesCarta">
-
+            <div class="contenedorDatos">
+                <div class="juegoHeader">
+                    <div class="backWidth Planta"></div>
+                    <h2 class="juegoH2"><span class="PlantaColor">A</span>ctualizaciones</h2>
+                    <img src="img/profesor_oak.png" alt="profesor Oak" width="32" height="48">
+                </div>
+                <div class="juegoDatos">
+                    <a class="enlaceDatosAct" href="">Change Log 04/11/23</a>
+                    <a class="enlaceDatosAct" href="">Halloween 2023</a>
+                    <a class="enlaceDatosAct" href="">Change Log 19/10/23</a>
+                    <a class="enlaceDatosAct" href="">HeartGold 12/08/23</a>
+                    <a class="enlaceDatosAct" href="">Change Log 28/04/23</a>
+                    <a class="enlaceDatosAct" href="">Año Nuevo Lunar 23</a>
+                </div>
             </div>
-            <div class="pokedexCarta">
-
+            <div class="contenedorDatos">
+                <div class="juegoHeader">
+                    <div class="backWidth Agua"></div>
+                    <h2 class="juegoH2"><span class="AguaColor">P</span>okedex</h2>
+                    <img src="img/profesor_oak.png" alt="profesor Oak" width="32" height="48">
+                </div>
+                <div class="juegoDatos">
+                    <a class="enlaceDatosPoke" href="">Instalar Juego</a>
+                    <a class="enlaceDatosPoke" href="">Objetos</a>
+                    <a class="enlaceDatosPoke" href="">Crianza</a>
+                    <a class="enlaceDatosPoke" href="">Tienda</a>
+                    <a class="enlaceDatosPoke" href="">Regiones</a>
+                    <a class="enlaceDatosPoke" href="">Bayas</a>
+                    <a class="enlaceDatosPoke" href="">Trading</a>
+                    <a class="enlaceDatosPoke" href="">Pokedex</a>
+                    <a class="enlaceDatosPoke" href="">Crafteo</a>
+                    <a class="enlaceDatosPoke" href="">PVP</a>
+                </div>
             </div>
-            <div class="objetosCarta">
-
+            <div class="contenedorDatos">
+                <div class="juegoHeader">
+                    <div class="backWidth Eléctrico"></div>
+                    <h2 class="juegoH2"><span class="EléctricoColor">O</span>bjetos</h2>
+                    <img src="img/profesor_oak.png" alt="profesor Oak" width="32" height="48">
+                </div>
+                <div class="juegoDatos">
+                    <a class="enlaceDatosObj" href="">Instalar Juego</a>
+                    <a class="enlaceDatosObj" href="">Objetos</a>
+                    <a class="enlaceDatosObj" href="">Crianza</a>
+                    <a class="enlaceDatosObj" href="">Tienda</a>
+                    <a class="enlaceDatosObj" href="">Regiones</a>
+                    <a class="enlaceDatosObj" href="">Bayas</a>
+                    <a class="enlaceDatosObj" href="">Trading</a>
+                    <a class="enlaceDatosObj" href="">Pokedex</a>
+                    <a class="enlaceDatosObj" href="">Crafteo</a>
+                    <a class="enlaceDatosObj" href="">PVP</a>
+                </div>
             </div>
-            <div class="regionesCarta">
-
+            <div class="contenedorDatos">
+                <div class="juegoHeader">
+                    <div class="backWidth Hielo"></div>
+                    <h2 class="juegoH2"><span class="HieloColor">R</span>egiones</h2>
+                    <img src="img/profesor_oak.png" alt="profesor Oak" width="32" height="48">
+                </div>
+                <div class="juegoDatos">
+                    <a class="enlaceDatosReg" href="">Instalar Juego</a>
+                    <a class="enlaceDatosReg" href="">Objetos</a>
+                    <a class="enlaceDatosReg" href="">Crianza</a>
+                    <a class="enlaceDatosReg" href="">Tienda</a>
+                    <a class="enlaceDatosReg" href="">Regiones</a>
+                    <a class="enlaceDatosReg" href="">Bayas</a>
+                    <a class="enlaceDatosReg" href="">Trading</a>
+                    <a class="enlaceDatosReg" href="">Pokedex</a>
+                    <a class="enlaceDatosReg" href="">Crafteo</a>
+                    <a class="enlaceDatosReg" href="">PVP</a>
+                </div>
             </div>
-            <div class="localizacionesCarta">
-
+            <div class="contenedorDatos">
+                <div class="juegoHeader">
+                    <div class="backWidth Tierra"></div>
+                    <h2 class="juegoH2"><span class="TierraColor">L</span>ocalizaciones</h2>
+                    <img src="img/profesor_oak.png" alt="profesor Oak" width="32" height="48">
+                </div>
+                <div class="juegoDatos">
+                    <a class="enlaceDatosLoc" href="">Instalar Juego</a>
+                    <a class="enlaceDatosLoc" href="">Objetos</a>
+                    <a class="enlaceDatosLoc" href="">Crianza</a>
+                    <a class="enlaceDatosLoc" href="">Tienda</a>
+                    <a class="enlaceDatosLoc" href="">Regiones</a>
+                    <a class="enlaceDatosLoc" href="">Bayas</a>
+                    <a class="enlaceDatosLoc" href="">Trading</a>
+                    <a class="enlaceDatosLoc" href="">Pokedex</a>
+                    <a class="enlaceDatosLoc" href="">Crafteo</a>
+                    <a class="enlaceDatosLoc" href="">PVP</a>
+                </div>
             </div>
-            <div class="npcsCarta">
-
+            <div class="contenedorDatos">
+                <div class="juegoHeader">
+                    <div class="backWidth Fantasma"></div>
+                    <h2 class="juegoH2"><span class="FantasmaColor">N</span>pc's</h2>
+                    <img src="img/profesor_oak.png" alt="profesor Oak" width="32" height="48">
+                </div>
+                <div class="juegoDatos">
+                    <a class="enlaceDatosNpc" href="">Instalar Juego</a>
+                    <a class="enlaceDatosNpc" href="">Objetos</a>
+                    <a class="enlaceDatosNpc" href="">Crianza</a>
+                    <a class="enlaceDatosNpc" href="">Tienda</a>
+                    <a class="enlaceDatosNpc" href="">Regiones</a>
+                    <a class="enlaceDatosNpc" href="">Bayas</a>
+                    <a class="enlaceDatosNpc" href="">Trading</a>
+                    <a class="enlaceDatosNpc" href="">Pokedex</a>
+                    <a class="enlaceDatosNpc" href="">Crafteo</a>
+                    <a class="enlaceDatosNpc" href="">PVP</a>
+                </div>
             </div>
-            <div class="mecanicasCarta">
-
+            <div class="contenedorDatos">
+                <div class="juegoHeader">
+                    <div class="backWidth Lucha"></div>
+                    <h2 class="juegoH2"><span class="LuchaColor">M</span>ecanicas</h2>
+                    <img src="img/profesor_oak.png" alt="profesor Oak" width="32" height="48">
+                </div>
+                <div class="juegoDatos">
+                    <a class="enlaceDatosMec" href="">Instalar Juego</a>
+                    <a class="enlaceDatosMec" href="">Objetos</a>
+                    <a class="enlaceDatosMec" href="">Crianza</a>
+                    <a class="enlaceDatosMec" href="">Tienda</a>
+                    <a class="enlaceDatosMec" href="">Regiones</a>
+                    <a class="enlaceDatosMec" href="">Bayas</a>
+                    <a class="enlaceDatosMec" href="">Trading</a>
+                    <a class="enlaceDatosMec" href="">Pokedex</a>
+                    <a class="enlaceDatosMec" href="">Crafteo</a>
+                    <a class="enlaceDatosMec" href="">PVP</a>
+                </div>
             </div>
-            <div class="eventosCarta">
-
+            <div class="contenedorDatos">
+                <div class="juegoHeader">
+                    <div class="backWidth Psíquico"></div>
+                    <h2 class="juegoH2"><span class="PsíquicoColor">E</span>ventos</h2>
+                    <img src="img/profesor_oak.png" alt="profesor Oak" width="32" height="48">
+                </div>
+                <div class="juegoDatos">
+                    <a class="enlaceDatosEve" href="">Instalar Juego</a>
+                    <a class="enlaceDatosEve" href="">Objetos</a>
+                    <a class="enlaceDatosEve" href="">Crianza</a>
+                    <a class="enlaceDatosEve" href="">Tienda</a>
+                    <a class="enlaceDatosEve" href="">Regiones</a>
+                    <a class="enlaceDatosEve" href="">Bayas</a>
+                    <a class="enlaceDatosEve" href="">Trading</a>
+                    <a class="enlaceDatosEve" href="">Pokedex</a>
+                    <a class="enlaceDatosEve" href="">Crafteo</a>
+                    <a class="enlaceDatosEve" href="">PVP</a>
+                </div>
             </div>
-            <div class="extensionesCarta">
-
+            <div class="contenedorDatos">
+                <div class="juegoHeader">
+                    <div class="backWidth Volador"></div>
+                    <h2 class="juegoH2"><span class="VoladorColor">A</span>dd-ons</h2>
+                    <img src="img/profesor_oak.png" alt="profesor Oak" width="32" height="48">
+                </div>
+                <div class="juegoDatos">
+                    <a class="enlaceDatosAdd" href="">Instalar Juego</a>
+                    <a class="enlaceDatosAdd" href="">Objetos</a>
+                    <a class="enlaceDatosAdd" href="">Crianza</a>
+                    <a class="enlaceDatosAdd" href="">Tienda</a>
+                    <a class="enlaceDatosAdd" href="">Regiones</a>
+                    <a class="enlaceDatosAdd" href="">Bayas</a>
+                    <a class="enlaceDatosAdd" href="">Trading</a>
+                    <a class="enlaceDatosAdd" href="">Pokedex</a>
+                    <a class="enlaceDatosAdd" href="">Crafteo</a>
+                    <a class="enlaceDatosAdd" href="">PVP</a>
+                </div>
             </div>
         </div>
 	</main>
